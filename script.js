d7dff93ca6343428f56d503ad8f4c2dda0076bfc
HidekiMichi <105813974+HidekiMichi@users.noreply.github.com>
2024-01-31 04:17:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your JS code here. If required.
+let inputs = document.querySelectorAll('.code');
+
+document.addEventListener('DOMContentLoaded', () => {
+    inputs[0].focus();
+});
+
+inputs[0].addEventListener('input', function() {
+    if (inputs[0].value !== '') {
+        inputs[1].focus();
+    }
+});
+
+for (let i = 1; i < inputs.length - 1; i++) {
+    inputs[i].addEventListener('input', () => {
+        if (inputs[i].value !== '') {
+            inputs[i + 1].focus();
+        } else {
+            inputs[i - 1].focus();
+        }
+    });
+}
+inputs[inputs.length - 1].addEventListener('input', () => {
+    if (inputs[inputs.length - 1].value === '') {
+        inputs[inputs.length - 2].focus();
+    }
+});
