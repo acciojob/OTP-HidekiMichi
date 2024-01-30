ebfb7151e8ae0617e1b5faf71420b12032efe43d
HidekiMichi <105813974+HidekiMichi@users.noreply.github.com>
2024-01-31 04:41:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,20 @@
 //your JS code here. If required.
-let inputs = document.querySelectorAll('.code');
+const codeInputs = document.querySelectorAll(".code");
 
-document.addEventListener('DOMContentLoaded', () => {
-    inputs[0].focus();
-});
+        codeInputs.forEach((input, index) => {
+            input.addEventListener("input", (event) => {
+                const inputValue = event.target.value;
 
-inputs[0].addEventListener('input', function() {
-    if (inputs[0].value !== '') {
-        inputs[1].focus();
-    }
-});
+                if (inputValue.length === 1) {
+                    if (index < codeInputs.length - 1) {
+                        codeInputs[index + 1].focus();
+                    }
+                }
+            });
 
-for (let i = 1; i < inputs.length - 1; i++) {
-    inputs[i].addEventListener('input', () => {
-        if (inputs[i].value !== '') {
-            inputs[i + 1].focus();
-        } else {
-            inputs[i - 1].focus();
-        }
-    });
-}
-inputs[inputs.length - 1].addEventListener('input', () => {
-    if (inputs[inputs.length - 1].value === '') {
-        inputs[inputs.length - 2].focus();
-    }
-});
+            input.addEventListener("keydown", (event) => {
+                if (event.key === "Backspace" && index > 0) {
+                    codeInputs[index - 1].focus();
+                }
+            });
+        });
